8b9cacea5f7b2db295558db54324b0d5e81f99db
Michael Kieser <michael.kieser@tesera.com>
2018-05-17 13:43:01 -0600

    removes unnecessary functions from ktpi_util: TMS neighbours, CR neighbours

--- R/ktpi_util.R
+++ R/ktpi_util.R
@@ -2,81 +2,6 @@ suppressMessages(library(raster, quietly = TRUE))
 suppressMessages(library(rgdal, quietly = TRUE))
 suppressMessages(library(jsonlite))
 
-# gets tile number from tile file path "/<folder>/<col>/<row>.jpg"
-getTMSFolderZoomColRowExt <- function(filePath) {
-    # filePath <- "/topo/feat/34/56.jpg"
-    # parse the path by folder structure: topo feat col row.jpg
-    filePathPrs <- unlist(strsplit(filePath,"[// /\\]"))
-    # get the folder path: topo/feat/
-    fileFolder <- filePathPrs[1]
-    for (i in 2:(length(filePathPrs)-2)) {
-        fileFolder <- paste(fileFolder, filePathPrs[i], sep = "/")
-    }
-    # fileFolder <- paste(fileFolder, "/", sep = "")
-    # tile row is the second last parse: col
-    tileCol <- as.integer(filePathPrs[length(filePathPrs)-1])
-    # filename.ext is the last parse: row.jpg
-    fileWExt <- filePathPrs[length(filePathPrs)]
-    # parse the filename.ext by .: row jpg
-    fileWExtPrs <- unlist(strsplit(fileWExt,"[//.]"))
-    # tile row is the first parse: row
-    tileRow <- as.integer(fileWExtPrs[1])
-    # file extension is the last parse: .jpg
-    fileExt <- paste("." , fileWExtPrs[length(fileWExtPrs)], sep = "")
-
-    return(list(fileFolder = fileFolder,
-        tileCol = tileCol,
-        tileRow = tileRow,
-        fileExt = fileExt))
-}
-
-# get neighbouring raster images within a kernelSize
-getTMSTileFileNeighbours <- function(fileFolder, tileCol, tileRow, fileExt, kernelSize) {
-    # gets filename
-    filePath <- file.path(fileFolder, tileCol, tileRow)
-    filePath <- paste(filePath, fileExt, sep = "")
-    # initializes neighbour list
-    neighbourFileList <- c(filePath)
-    # gets raster
-    r <- raster(filePath)
-    # gets the smallest dimension (x OR y) of the current tile
-    xDim <- ncol(r) * xres(r)
-    yDim <- nrow(r) * xres(r)
-    minDim <- xDim
-    if (minDim > yDim) minDim <- yDim
-
-    # gets default kernelSize if not defined
-    if (is.null(kernelSize)) {
-        kernelSize <- minDim
-    } else {
-        kernelSize <- as.integer(kernelSize)
-    }
-
-    # gets the number of tile rings to cover the buffer kernelSize
-    tileRing <- ceiling (kernelSize / minDim)
-
-    # gets all the tiles in the kernelSize
-    # iterates from current tile column - tileRing to current tile column + tileRing
-    for (col in (-1 * tileRing):tileRing) {
-        neighbourCol <- tileCol + col
-        # iterates from current tile row - tileRing to current tile row + tileRing
-        for (row in (-1 * tileRing):tileRing) {
-            neighbourRow <- tileRow + row
-            neighbour <- file.path(fileFolder, neighbourCol, neighbourRow)
-            neighbour <- paste(neighbour, fileExt, sep = "")
-            # print(neighbour)
-            # adds neighbour to list if neighbour col or row >=0, there are no negative col/row tiles
-            if (neighbourCol >= 0 & neighbourRow >= 0) {
-                neighbourFileList <- c(neighbourFileList, neighbour)
-            }
-        }
-    }
-    # removes duplicate neighbours (ie. current tile)
-    neighbourFileList <- sort(unique(neighbourFileList))
-    # return list of tile file neighbours
-    return(neighbourFileList)
-}
-
 # merges neighbour rasters in a list
 mergeRasters <- function(neighbourFileList) {
     # initializes mergedNeighbourRasters raster
@@ -156,41 +81,6 @@ createFolder <- function(folderPath, newFolder) {
     } else {
         dir.create(file.path(folderPath, newFolder))
     }
-}
-
-# get neighbouring raster images within a kernelSize, and within a min&max col&row
-getNeighbourhoodCR <- function(tileCol, tileRow, minCol, maxCol, minRow, maxRow, 
-    rasterCells, rasterCellSize, kernelSize) {
-    tileCol <- as.integer(tileCol)
-    tileRow <- as.integer(tileRow)
-    minCol <- as.integer(minCol)
-    maxCol <- as.integer(maxCol)
-    minRow <- as.integer(minRow)
-    maxRow <- as.integer(maxRow)
-    # initializes neighbour list
-    neighbourhoodCR <- c()
-    # gets the raster dimension
-    rasterSize <- as.integer(rasterCells) * as.integer(rasterCellSize)
-    # gets the number of tile rings to cover the buffer kernelSize
-    tileRing <- ceiling (as.integer(kernelSize) / rasterSize)
-
-    # gets all the row,col in the kernelSize
-    # iterates from current tile column - tileRing to current tile column + tileRing
-    for (col in (-1 * tileRing):tileRing) {
-        neighbourCol <- as.integer(tileCol) + col
-        # iterates from current tile row - tileRing to current tile row + tileRing
-        for (row in (-1 * tileRing):tileRing) {
-            neighbourRow <- as.integer(tileRow) + row
-            # adds neighbour to list if neighbour col or row >= min col or row AND col or row <= max col or row
-            if ((neighbourCol >= minCol & neighbourCol <= maxCol & neighbourRow >= minRow & neighbourRow <= maxRow)) {
-                neighbour <- paste(neighbourCol, neighbourRow, sep = "/")
-                neighbourhoodCR <- c(neighbourhoodCR, neighbour)
-            }
-        }
-    }
-    neighbourhoodCRchar <- paste(as.character(neighbourhoodCR), collapse = ";")
-    # return data frame of tile file neighbours
-    return(neighbourhoodCRchar)
 }
 
 getNeighbours <- function(prepend = "", tilecol = NULL, tilerow = NULL, append = "", tilecolmin = NULL, tilecolmax = NULL, tilerowmin = NULL, tilerowmax = NULL, rastercells = NULL, rastercellsize = NULL, neighbourhood = NULL, tiles) {
@@ -285,8 +175,6 @@ createKtpiCLICommands <- function(ktpiFunction, featureFolder, demFolder, extens
                         for (size in 1:sizeCount) {
                             kernels <- seq(kernelFrom[size], kernelTo[size], by = kernelStep[size])
                             for (kernel in kernels) {
-                                neighbourhoodCR <- getNeighbourhoodCR(col, row, tileColMin, tileColMax, tileRowMin, tileRowMax, 
-                                    rasterCells, rasterCellSize, kernel)
                                 for (orient in orientations) {
                                     # orientation <- paste("-o", orient, sep = " ")
                                     # additionalArgs <- paste("-d", demCalcSize[size], "-k", kernel, orientation, exportRasters, sep = " ")
@@ -360,8 +248,6 @@ createKtpiJSONMessages <- function(ktpiFunction, featureFolder, demFolder, exten
                         for (size in 1:sizeCount) {
                             kernels <- seq(kernelFrom[size], kernelTo[size], by = kernelStep[size])
                             for (kernel in kernels) {
-                                neighbourhoodCR <- getNeighbourhoodCR(col, row, tileColMin, tileColMax, tileRowMin, tileRowMax, 
-                                    rasterCells, rasterCellSize, kernel)
                                 for (orient in orientations) {
                                     # orientation <- paste("-o", orient, sep = " ")
                                     ktpiJSONMessage <- data.frame(getdata = character(1), runcommand = character(1), stringsAsFactors = FALSE)
